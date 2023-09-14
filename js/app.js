539476dad9d5bb9cc5474b32b4aeb747d4db50fc
Minha Kwon <namisoft@naver.com>
2023-09-15 01:24:56 +0900

    컨트롤러 기능 구현

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-import { smoothScrollTo } from "./utils.js";
+var tt = "tt";
+
+const clickBtn = (stair) => {
+  console.log(stair);
+  switch (stair) {
+    case "L":
+      smoothScrollTo("section1");
+      break;
+    case "B1":
+      smoothScrollTo("section2");
+      break;
+    case "B2":
+      smoothScrollTo("section3");
+      break;
+    case "B3":
+      smoothScrollTo("section4");
+      break;
+    default:
+      smoothScrollTo("section1");
+  }
+};
+
+const smoothScrollTo = (containerByID) => {
+  const node = document.querySelector(`#${containerByID}`);
+  const navbarHeight = 0;
+
+  window.scrollTo({
+    top: node.offsetTop - navbarHeight,
+    behavior: "smooth",
+  });
+};
--- js/utils.js
+++
@@ -1,9 +0,0 @@
-export const smoothScrollTo = (containerByID) => {
-  const node = document.querySelector(`#${containerByID}`);
-  const navbarHeight = 102;
-
-  window.scrollTo({
-    top: node.offsetTop - navbarHeight,
-    behavior: "smooth",
-  });
-};
