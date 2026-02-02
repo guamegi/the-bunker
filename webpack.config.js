2811eb94940d790b89d371499742b604cbb559fb
Minha Kwon <namisoft@naver.com>
2026-02-02 18:16:31 +0900

    웹팩 설정 변경

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/js/main.js",
-  output: {
-    filename: "main.js",
-    path: `${__dirname}/dist`,
-  },
+// env(환경변수), argv(인자값)를 파라미터로 받는 함수형태로 변경
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: argv.mode, // 명령어가 넘겨준 'production' 또는 'development' 적용
+    entry: "./src/js/main.js",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+    },
+    // 배포 모드일 때만 보안용 소스맵 적용, 개발 때는 빠른 디버깅용 적용
+    devtool: isProduction ? "hidden-source-map" : "eval-source-map",
+  };
 };
